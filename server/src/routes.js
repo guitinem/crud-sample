98de730f94ead4bf38d2274aab32a53c0cd4eafd
Guilherme Tinem <guizstinem@gmail.com>
2026-02-12 16:59:17 -0300

    feat: add MongoDB Docker configuration and user management routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,20 @@
 const { Router } = require("express");
+const UserController = require("./controllers/UserController");
+const AuthController = require("./controllers/AuthController");
+const authMiddleware = require("./middlewares/auth");
 
 const routes = Router();
 
 routes.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: "API SPS - User Management" });
 });
+
+routes.post("/auth", AuthController.login);
+routes.get("/me", authMiddleware, AuthController.me);
+routes.post("/users", UserController.store);
+routes.get("/users", authMiddleware, UserController.index);
+routes.get("/users/:id", authMiddleware, UserController.show);
+routes.put("/users/:id", authMiddleware, UserController.update);
+routes.delete("/users/:id", authMiddleware, UserController.destroy);
+
+module.exports = routes;
